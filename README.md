58367fd7a67b08f71c74f4cff1a79ba2144654fe
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-11-04 23:38:22 +0530

    🧑‍💻DARK MD🧑‍💻

---
+++ README.md
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+<body>
+  <p align="center">
+    <a href="https://chat.whatsapp.com/IpDbNkTpz1l520HHFuS7B7">
+      <img alt="Support" height="300"
+        src="https://telegra.ph/file/d56271ce89ab876ba00b8.jpg">
+    </a>
+  </p>
+  <h1 align="center">MAC-𝙼𝙳 𝙼𝚄𝙻𝚃𝙸 𝙳𝙴𝚅𝙸𝙲𝙴 𝙱𝙾𝚃</h1>
+  <p align="center">
+    <div hx-get="/visitor_count" hx-target="this" hx-swap="innerHTML"></div>
+  </p>
+  <p align="center">
+    <a href="https://chat.whatsapp.com/BEhEne7RdGBC3y5vYltuxL" target="_blank">
+      <img alt="whatsapp Group" src="https://img.shields.io/badge/ MAC Support Group -25D366?style=for-the-badge&logo=whatsapp&logoColor=blue" />
+    </a>
+  </p>
+---
+Scan QR Code
+
+1. ***Get [SESSION-ID](https://replit.com/@maccoder74/MAC-Md?v=1) by scanning QR code. `Whatapp>Three dots>Linked Devices`***
+--- 
+# ```Fork Repo```
+2. ***Click [FORK](https://github.com/Maccoder3/MAC-MD/fork)***
+# ```WATCH TUTORIAL```
+3. ***WATCH [TUTORIAL](https://youtu.be/t6dvww00lQA?si=mOdD154N3Tgq5IK5)***
+---
+  <hr>
+  <h2>Deployments METHODS</h2>
+  <details close>
+    <summary>Click to choose your favorite platform to deploy</summary>
+    <br><br>
+    <h4 align="center">Deploy on Repl.it</h4>
+    <p align="center">
+      <a href="https://repl.it/github/Maccoder3/MAC-MD">
+        <img src="https://repl.it/badge/github/quiec/whatsasena" width="170px" alt="Deploy on REPLIT">
+      </a>
+    </p>
+    <br>
+    <h4 align="center">Deploy on CodesSpace</h4>
+    <p align="center">
+      <a href="https://github.com/codespaces/new">
+        <img src="https://img.shields.io/badge/DEPLOY CODESPACE-h?color=red&style=for-the-badge&logo=visualstudiocode" width="170px" alt="Deploy on CodesSpaces">
+      </a>
+    </p>
+    <br>
+    <h4 align="center">Deploy on Heroku</h4>
+    <p align="center">
+      <a href="https://heroku.com/deploy?template=https://github.com/Maccoder3/MAC-MD">
+        <img src="https://www.herokucdn.com/deploy/button.png" width="170px" alt="Deploy on Heroku">
+      </a>
+    </p>
+    <br>
+    <h4 align="center">Deploy on RailWay</h4>
+    <p align="center">
+      <a href="https://railway.app/new">
+        <img src="https://railway.app/button.svg" alt="Deploy on Railway" width="170px">
+      </a>
+    </p>
+    <br>
+    <h4 align="center">Deploy on Mogenius</h4>
+    <p align="center">
+      <a href="https://studio.mogenius.com/">
+        <img src="https://www.cloudflare.com/static/90073b1e5bd8a0765640a20febb3dc22/mogenius_logo_quer.png" alt="Deploy on Mogenius" width="170px">
+      </a>
+    </p>
+    <br>
+    <h4 align="center">Deploy on Uffizzi</h4>
+<p align="center">
+    <a href="https://www.uffizzi.com/">
+    <img src="https://i.ibb.co/Y29Kv4X/Screenshot-195.png" alt="Deploy on Uffizzi" width="125px">
+    </a>
+    
+</p>
+
+<br>
+
+<h4 align="center"> Deploy on BoxMineWorld
+</h4>
+  
+<p align="center">
+    <a href="https://dash.boxmineworld.com/">
+    <img src="https://graph.org/file/2af0e67f320986702ea24.jpg" alt="Deploy on Boxmineworld" width="175px">
+    </a>
+    <br>
+
+</p>
+
+<p align="center" >
+    <br>
+    __________________________
+    <br>
+</p>
+
+
+
+</details>
+
+<br>
---
+++ config.js
@@ -0,0 +1,58 @@
+const fs = require('fs-extra')
+if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/config.env' })
+
+
+//═══════[Required Variables]════════\\
+global.owner = process.env.OWNER_NUMBER.split(",")
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority"
+global.port= process.env.PORT || 5000
+global.email = 'sam@secktor.live'
+global.github = 'https://github.com/Maccoder3/MAC-MD'
+global.location = 'Sultanpur IN'
+global.gurl = 'https://instagram.com/' // add your username
+global.sudo = process.env.SUDO || '256705036288'
+global.devs = '256705036288';
+global.website = 'https://github.com/Maccoder3/MAC-MD' //wa.me/+25000000000000
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/8ec95da555e8347af71bd.jpg'
+module.exports = {
+  botname:   process.env.BOT_NAME === undefined ? 'MAC 𝐁𝐨𝐭' : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? 'mac' : process.env.OWNER_NAME,
+  sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
+  author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'mac' : process.env.PACK_INFO.split(";")[0],
+  auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
+  packname:  process.env.PACK_INFO.split(";")[1] === undefined ? 'MAC-Md' : process.env.PACK_INFO.split(";")[1],
+  autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
+  antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
+  alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
+  antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '971' : process.env.FAKE_COUNTRY_CODE,
+  readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE,
+  auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER,
+  HANDLERS:  process.env.PREFIX === undefined ? ['.'] : process.env.PREFIX,
+  warncount : process.env.WARN_COUNT === undefined ? 3 : process.env.WARN_COUNT,
+  disablepm:  process.env.DISABLE_PM === undefined ? false : process.env.DISABLE_PM,
+  levelupmessage:  process.env.LEVEL_UP_MESSAGE === undefined ? false : process.env.LEVEL_UP_MESSAGE,
+  antilink:  process.env.ANTILINK_VALUES === undefined ? 'chat.whatsapp.com' : process.env.ANTILINK_VALUES,
+  antilinkaction: process.env.ANTILINK_ACTION === undefined ? 'remove' : process.env.ANTILINK_ACTION,
+  BRANCH: 'main', 
+  ALIVE_MESSAGE:  process.env.ALIVE_MESSAGE === undefined ? '' : process.env.ALIVE_MESSAGE,
+  autobio:  process.env.AUTO_BIO === undefined ? false : process.env.AUTO_BIO,
+  OPENAI_API_KEY:  process.env.OPENAI_API_KEY === undefined ? false : process.env.OPENAI_API_KEY,
+  heroku:  process.env.heroku === undefined ? false : process.env.heroku,
+  HEROKU: {
+    HEROKU: process.env.HEROKU ||false,
+    API_KEY: process.env.HEROKU_API_KEY === undefined ? '' : process.env.HEROKU_API_KEY,
+    APP_NAME: process.env.HEROKU_APP_NAME === undefined ? '' : process.env.HEROKU_APP_NAME
+},
+  VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
+  LANG: process.env.THEME|| 'MAC',
+  WORKTYPE: process.env.WORKTYPE === undefined ? 'private' : process.env.WORKTYPE
+};
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(`Update'${__filename}'`)
+    delete require.cache[file]
+	require(file)
+})
