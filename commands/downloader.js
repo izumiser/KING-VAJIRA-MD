ce9c8cfa76a175c33271bdf31d041f4da5b947c9
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-11-05 11:53:52 +0530

    🧑‍💻DARK MD🧑‍💻

--- commands/downloader.js
+++ commands/downloader.js
@@ -291,7 +291,7 @@ cmd({
             if (infoYt.videoDetails.lengthSeconds >= videotime) return citel.reply(`😔 Video file too big!`);
             let titleYt = infoYt.videoDetails.title;
             let randomName = getRandom(".mp3");
-            citel.reply('*Downloadig:* '+titleYt)
+            citel.reply('*📥Downloadig:* '+titleYt)
             const stream = ytdl(anu.url, {
                     filter: (info) => info.audioBitrate == 160 || info.audioBitrate == 128,
                 })
