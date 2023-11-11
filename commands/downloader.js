74e16bf9e413165bfc738b376acae39bd8795188
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-11-11 08:43:30 +0530

    👑 ＫＩＮＧ - ＶＡＪＩＲＡ 👑

--- commands/downloader.js
+++ commands/downloader.js
@@ -62,6 +62,50 @@ cmd({
   else return await citel.reply("Error While Downloading Your Video")  
  }) 
      //---------------------------------------------------------------------------
+cmd({ 
+             pattern: "facebook", 
+             alias :  ['fb','fbdl','ෆෙසබුක්'], 
+             desc: "Downloads fb videos  .", 
+             category: "downloader", 
+             filename: __filename, 
+             use: '<add fb url.>' 
+         }, 
+  
+         async(Void, citel, text) => { 
+ Void.sendMessage(citel.chat, {  
+               react: {  
+                   text: "🎬",  
+                   key: citel.key  
+               }  
+           })  
+ if(!text) return citel.reply(`*_Please Give me Facebook Video Url_*`); 
+ fbInfoVideo.getInfo(text) 
+   .then(info =>{ 
+ let vurl=info.video.url_video; 
+  
+       let data  ="*Video Name     :* "+  info.video.title; 
+           data +="\n*Video Views    :* "+  info.video.view; 
+           data +="\n*Video Comments :* "+  info.video.comment; 
+           data +="\n*Video Likes    :* "+info.video.reaction.Like ; 
+  
+                         let buttonMessage = { 
+                         video: {url:vurl}, 
+                         mimetype: 'video/mp4', 
+                         fileName: info.video.title+`.mp4`, 
+                         caption :"     *FACEBOOK DOWNLOADER*  \n"+data 
+  
+                     } 
+                  Void.sendMessage(citel.chat, buttonMessage, { quoted: citel }); 
+  
+  
+  
+ }) 
+   .catch(err =>{ 
+             citel.reply("Error, Video Not Found\n *Please Give Me A Valid Url*"); 
+             console.error(err); 
+           }) 
+  })
+     //---------------------------------------------------------------------------
      cmd({
         pattern: "yts",
         desc: "Gives descriptive info of query from youtube..",
